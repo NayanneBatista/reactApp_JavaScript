4f582e145a438dd1f1971bf10759704ec48d000e
Nayanne Batista <nayanne.lb@gmail.com>
2021-02-26 11:13:46 -0300

    added onClick to complete Challenge on succeeded button

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -5,7 +5,7 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 
 export function ChallengeBox() {
   
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
 
     return(
         <div className={styles.challengeBoxContainer}>
@@ -23,6 +23,7 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={completeChallenge}
                         >
                             Completei
                         </button>
