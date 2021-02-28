3070e998cad2139149e0fc96afe363e43b2fd3ad
Nayanne Batista <nayanne.lb@gmail.com>
2021-02-27 22:22:19 -0300

    update LevelUpModal

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface Challenge {
     type: 'body' | 'eye';
@@ -35,6 +36,7 @@ export function ChallengesProvider({ children,  ...rest}: ChallengesProviderProp
     const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
     const [activeChallenge, setActiveChallenge] = useState(null)
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false)
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
@@ -55,6 +57,7 @@ export function ChallengesProvider({ children,  ...rest}: ChallengesProviderProp
 
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelUpModalOpen(true)
     }
 
     function startNewChallenge() {
@@ -110,6 +113,8 @@ export function ChallengesProvider({ children,  ...rest}: ChallengesProviderProp
             }}
         >
             { children }
+
+            { isLevelUpModalOpen && <LevelUpModal />}
         </ChallengesContext.Provider>
   );
 }
